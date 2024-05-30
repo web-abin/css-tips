8239e661022659dead72c37afbf33d54a7add3c1
王俊彬 <1872495742@qq.com>
2024-05-30 16:49:03 +0800

    feat: 外链跳转

--- js/layout.js
+++ js/layout.js
@@ -9,8 +9,8 @@ document.getElementById('header').innerHTML = `
 
 document.getElementById('footer').innerHTML = `
 <footer>
-  <h2>Create by <a href="https://github.com/web-abin"/>web-abin</a></h2>
-  <a href="https://github.com/web-abin"/>前端助手</a>
-  <a href="https://juejin.cn/user/994399097982728"/>掘金主页</a>
-  <a href="https://blog.csdn.net/qq_38974163?type=blog"/>CSDN</a>
+  <h2>Create by <a href="https://github.com/web-abin" rel="nofollow external" target="_blank"/>web-abin</a></h2>
+  <a href="https://github.com/web-abin" rel="nofollow external" target="_blank"/>前端助手</a>
+  <a href="https://juejin.cn/user/994399097982728" rel="nofollow external" target="_blank"/>掘金主页</a>
+  <a href="https://blog.csdn.net/qq_38974163?type=blog" rel="nofollow external" target="_blank"/>CSDN</a>
 </footer>`
