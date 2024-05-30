17e7392a8459b6d58541923775f9543e02621e2d
王俊彬 <1872495742@qq.com>
2024-05-30 16:20:54 +0800

    feat

--- js/layout.js
+++ js/layout.js
@@ -2,8 +2,8 @@ document.getElementById('header').innerHTML = `
 <header>
   <h1>CSS Tips</h1>
   <nav>
-    <a href="/">首页</a>
-    <a href="/about.html">关于我</a>
+    <a href="./">首页</a>
+    <a href="./about.html">关于我</a>
   </nav>
 </header>`
 
