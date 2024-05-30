4a2350b5c062f3cb1444e5685995fccdc63b9eef
王俊彬 <1872495742@qq.com>
2024-05-30 12:11:24 +0800

    test

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/home/index.vue'),
+    },
+    {
+      path: '/test',
+      name: 'index',
+      component: () => import('@/views/test/index.vue'),
+      meta: { title: 'MBTI测试', describe: '首页结果页' }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/views/error/index.vue')
+    }
+  ],
+})
+
+
+export default router
