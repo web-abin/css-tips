6bb725f6bb826d7ef35bb5f740275847947dc9e8
王俊彬 <1872495742@qq.com>
2024-05-30 16:54:23 +0800

    fix: 首页时间格式化

---
+++ js/utils.js
@@ -0,0 +1,14 @@
+// 时间格式化  DD-MM-DD hh:mm:ss
+function formatDate(time) {
+  let _time = new Date(time)
+
+  let Y = _time.getFullYear()
+  let M = _time.getMonth() + 1
+  let D = _time.getDate()
+  return `${Y}-${startWith(M)}-${startWith(D)}`
+}
+
+// 前面补0
+function startWith(num) {
+  return num >= 10 ? num : '0' + num
+}
