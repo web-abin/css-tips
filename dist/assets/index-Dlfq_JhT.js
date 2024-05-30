ad780533cf8113d3eb0778ce4fbde45c2c32f017
王俊彬 <1872495742@qq.com>
2024-05-30 12:22:37 +0800

    build

---
+++ dist/assets/index-Dlfq_JhT.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-vM1KFqrR.js";const o={};function r(t,s){return n(),c("div",null,"列表")}const a=e(o,[["render",r]]);export{a as default};
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/css-tips/',
   plugins: [vue()],
   resolve: {
     alias: {
